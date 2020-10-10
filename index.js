317689122252754d1bb6713d036a5f8351f4ef93
lambdagit101 <48295237+lambdagit101@users.noreply.github.com>
2020-10-11 01:59:21 +0300

    added thingies

--- index.js
+++ index.js
@@ -1,11 +1,18 @@
-var irc = require('irc');
-const prompt = require('prompt');
+var irc = require('irc'); // Loading the IRC functions so we have a working IRC client
+const prompt = require('prompt'); // Loading the prompt function so we can send messages
 const ircserver = require('./ircinfo.json').server;
 const ircnick = require('./ircinfo.json').nickname;
 const ircchannel = require('./ircinfo.json').channel;
+const ircport = require('./ircinfo.json').ircport;
+const ircrealname = require('./ircinfo.json').realname;
+const ircusername = require('./ircinfo.json').username;
+const prefix = require('./ircinfo.json').commandprefix;
 
 var client = new irc.Client(ircserver, ircnick, {
     channels: [ircchannel],
+	port: ircport,
+	realName: ircrealname,
+	userName: ircusername
 });
 
 console.log(`Logging in to ${ircserver} with nickname ${ircnick} on channel ${ircchannel}...`);
@@ -20,7 +27,13 @@ function sendmessage() {
 	prompt.start();
 	
 	prompt.get(properties, function (err, result) {
-		client.say(ircchannel, result.Message);
+		if (result.Message.toLowerCase().startsWith(`${prefix}disconnect`)) {
+			console.log('Quitting...');
+			client.part(ircchannel);
+			process.exit();	
+		} else {
+			client.say(ircchannel, result.Message);
+		}
 		sendmessage();
 	});
 }
@@ -52,7 +65,15 @@ client.addListener('quit', function (nick, reason, channels, message) {
 });
 
 client.addListener('kick', function (channel, nick, by, reason, message) {
-    console.log(nick + ' was kicked from ' + channel + ' by ' + by + '. Reason: ' + reason);
+    console.log(nick + ' was kicked from ' + ircserver + ' by ' + by + '. Reason: ' + reason);
+});
+
+client.addListener('kill', function (nick, reason, channel, message) {
+    console.log(nick + ' was killed from ' + ircserver + '. Reason: ' + reason);
+});
+
+client.addListener('nick', function (oldnick, newnick, channels, message) {
+    console.log(oldnick + ' changed nickname to ' + newnick + '.');
 });
 
 client.addListener('registered', function (message) {
